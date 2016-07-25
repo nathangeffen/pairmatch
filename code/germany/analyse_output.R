484b972eb20e0232d7c604047972d31d7be78e65
Nathan Geffen <nathangeffen@gmail.com>
2016-07-25 21:01:44 +0200

    Prepare for production run.

---
+++ code/germany/analyse_output.R
@@ -0,0 +1,104 @@
+# Cluster shuffle: Determine best k for each value of n (number of agents) and generate plot
+
+nvals = c(10000,50000,100000,250000,500000,750000,1000000)
+
+bestk = c()
+for (n in nvals) {
+  filename = paste("output_var_C_", format(n, scientific=FALSE), ".csv", sep="")
+  fr <- read.csv(filename)
+  m <- max(fr$rate)
+  L <- fr$rate == m
+  fr <- fr[L,]
+  k <- min(fr$k)
+  bestk <- c(bestk, k)
+}
+print(data.frame(n=nvals,k=bestk))
+bestkcluster = bestk # Save for comparison with Distribution match
+
+options(scipen=2)
+png('1_plotClusterShuffleBestK.png')
+plot(nvals, bestk, type="b", main="Cluster shuffle",
+      sub="Best number of neighbours for number of agents",
+      ylab="Best value for k (neighbours)",  xlab="Number of agents (n)")
+dev.off()
+
+# Cluster shuffle: Determine best c for each value of n (number of agents) and generate plot
+
+bestc = c()
+for (n in nvals) {
+  filename = paste("output_var_C_", format(n, scientific=FALSE), ".csv", sep="")
+  fr <- read.csv(filename)
+  m <- max(fr$rate)
+  L <- fr$rate == m
+  fr <- fr[L,]
+  c <- min(fr$c)
+  bestc <- c(bestc, c)
+}
+
+print(data.frame(n=nvals,c=bestc))
+options(scipen=2)
+png('2_plotClusterShuffleBestC.png')
+plot(nvals, bestc, type="b", main="Cluster shuffle", sub="Best number of clusters for number of agents",
+     ylab="Best value for c (clusters)",  xlab="Number of agents (n)")
+dev.off()
+
+# Cluster shuffle: Determine lowest c at best value of k and generate plot
+l = length(nvals)
+outfr = data.frame(k=1:l,c=1:l,n=1:l)
+i = 1
+for (n in nvals) {
+  filename = paste("output_var_C_", format(n, scientific=FALSE), ".csv", sep="")
+  fr <- read.csv(filename)
+  m <- max(fr$rate)
+  L <- fr$rate == m
+  fr <- fr[L,]
+  k <- min(fr$k)
+  L <- fr$k == k
+  fr <- fr[L,]
+  c = min(fr$c)
+  row = c(k,c,n)
+  outfr[i,] = c(k,c,n)
+  i <- i + 1
+}
+print(outfr)
+
+options(scipen=2)
+png('3_plotClusterShuffleBestKC.png')
+plot(nvals, outfr$k, type="b", main="Cluster shuffle", sub="Best number of clusters for number of agents",
+     ylab="Best value for k (red) and c (green)",  xlab="Number of agents (n)", col="red")
+lines(nvals,outfr$c,col="green", type="b")
+dev.off()
+
+# Distribution v Cluster: Determine best k for each value of n (number of agents) and generate plot
+
+bestk = c()
+for (n in nvals) {
+  filename = paste("output_var_D_", format(n, scientific=FALSE), ".csv", sep="")
+  fr <- read.csv(filename)
+  m <- max(fr$rate)
+  L <- fr$rate == m
+  fr <- fr[L,]
+  k <- min(fr$k)
+  bestk <- c(bestk, k)
+}
+print(data.frame(n=nvals,k=bestk))
+
+options(scipen=2)
+png('4_plotClusterShuffleVsDistributionBestK.png')
+plot(nvals, bestk, type="b", main="Ideal number of neighbours",
+     sub="Cluster shuffle (green) vs Distribution (red)",
+     ylab="Best value for k (neighbours)",  xlab="Number of agents (n)", col="red")
+lines(nvals,bestkcluster,col="green", type="b")
+dev.off()
+
+# Cluster shuffle: k,c, success
+
+library(scatterplot3d)
+fr <- read.csv("output_var_C_1000000.csv")
+attach(fr)
+k = fr$k
+c = fr$c
+rate = fr$rate * 100 # percent correct
+png('5_plotClusterShuffleKCRate.png')
+scatterplot3d(k,c,rate)
+dev.off()
--- code/germany/run_tests.sh
+++ code/germany/run_tests.sh
@@ -14,8 +14,14 @@ mv agent_list.csv agent_input_50000.csv
 Rscript step2b_simmatch_nathan_edits.R 100000
 mv agent_list.csv agent_input_100000.csv
 
+Rscript step2b_simmatch_nathan_edits.R 250000
+mv agent_list.csv agent_input_250000.csv
+
 Rscript step2b_simmatch_nathan_edits.R 500000
 mv agent_list.csv agent_input_500000.csv
+
+Rscript step2b_simmatch_nathan_edits.R 750000
+mv agent_list.csv agent_input_750000.csv
 
 Rscript step2b_simmatch_nathan_edits.R 1000000
 mv agent_list.csv agent_input_1000000.csv
@@ -26,7 +32,11 @@ mv agent_list.csv agent_input_1000000.csv
 
 ./germany_partners-rel -i agent_input_100000.csv -s $RANDOM -r 30 -k 1400 -c 100 >output_fixed_100000.csv &
 
+./germany_partners-rel -i agent_input_250000.csv -s $RANDOM -r 30 -k 1400 -c 100 >output_fixed_250000.csv &
+
 ./germany_partners-rel -i agent_input_500000.csv -s $RANDOM -r 30 -k 2000 -c 100 >output_fixed_500000.csv &
+
+/germany_partners-rel -i agent_input_750000.csv -s $RANDOM -r 30 -k 2500 -c 100 >output_fixed_750000.csv &
 
 ./germany_partners-rel -i agent_input_1000000.csv -s $RANDOM -r 30 -k 3500 -c 100 >output_fixed_1000000.csv &
 
@@ -38,7 +48,11 @@ wait
 
 ./germany_partners-rel -i agent_input_100000.csv -s $RANDOM -k 50 -c 50 -vk 40 -vc 40 -vt 30 -r 900 -a C >output_var_C_100000.csv &
 
+./germany_partners-rel -i agent_input_250000.csv -s $RANDOM -k 80 -c 80 -vk 60 -vc 60 -vt 30 -r 900 -a C >output_var_C_250000.csv &
+
 ./germany_partners-rel -i agent_input_500000.csv -s $RANDOM -k 100 -c 100 -vk 80 -vc 80 -vt 30 -r 900 -a C >output_var_C_500000.csv &
+
+./germany_partners-rel -i agent_input_750000.csv -s $RANDOM -k 90 -c 90 -vk 90 -vc 90 -vt 35 -r 1225 -a C >output_var_C_750000.csv &
 
 ./germany_partners-rel -i agent_input_1000000.csv -s $RANDOM -k 100 -c 100 -vk 100 -vc 100 -vt 40 -r 1600 -a C >output_var_C_1000000.csv &
 
@@ -50,7 +64,11 @@ wait
 
 ./germany_partners-rel -i agent_input_100000.csv -s $RANDOM -k 50 -vk 40 -r 50 -a D >output_var_D_100000.csv &
 
+./germany_partners-rel -i agent_input_250000.csv -s $RANDOM -k 80 -vk 60 -r 50 -a D >output_var_D_250000.csv &
+
 ./germany_partners-rel -i agent_input_500000.csv -s $RANDOM -k 100 -vk 80 -r 50 -a D >output_var_D_500000.csv &
+
+./germany_partners-rel -i agent_input_750000.csv -s $RANDOM -k 100 -vk 90 -r 50 -a D >output_var_D_750000.csv &
 
 ./germany_partners-rel -i agent_input_1000000.csv -s $RANDOM -k 100 -vk 100 -r 50 -a D >output_var_D_1000000.csv &
 
