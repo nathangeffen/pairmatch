c8d8c74b78e6777065cc5e7c2b5315a47cf44d8f
Nathan Geffen <nathangeffen@gmail.com>
2016-08-04 15:05:27 +0200

    Added test script for large file.

---
+++ code/germany/test_large.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# To skip recreation of all agent input files, run this script as:
+# $ ./run_tests skip
+
+# Stop on error
+set -v
+set -x
+set -e
+
+echo "Starting time: "
+date
+
+./germany_partners-rel -i agent_input_80620000.csv -s $RANDOM -k 10000 -c 5000 -a DC >output_fixed_1000000.csv &
+
+
+echo "Ending at:"
+date
