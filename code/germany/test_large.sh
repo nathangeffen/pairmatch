93a2aca162389231182d4d434affd2acef172abd
Nathan Geffen <nathangeffen@gmail.com>
2016-08-04 22:35:07 +0200

    Modified large file test to just use cluster shuffle with variations of k and c running in separate processes.

--- code/germany/test_large.sh
+++ code/germany/test_large.sh
@@ -13,11 +13,23 @@ date
 
 ./germany_partners-rel -i agent_input_80620000.csv -s $RANDOM -k 30000 -c 10000 -a C >output_fixed_C_80620000_k_30000_c_10000.csv &
 
-./germany_partners-rel -i agent_input_80620000.csv -s $RANDOM -k 30000 -a D >output_fixed_D_80620000_k_30000.csv &
+./germany_partners-rel -i agent_input_80620000.csv -s $RANDOM -k 35000 -c 10000 -a C >output_fixed_C_80620000_k_35000_c_10000.csv &
+
+./germany_partners-rel -i agent_input_80620000.csv -s $RANDOM -k 40000 -c 10000 -a C >output_fixed_C_80620000_k_40000_c_10000.csv &
+
+./germany_partners-rel -i agent_input_80620000.csv -s $RANDOM -k 45000 -c 6000 -a C >output_fixed_C_80620000_k_45000_c_6000.csv &
+
+./germany_partners-rel -i agent_input_80620000.csv -s $RANDOM -k 45000 -c 8000 -a C >output_fixed_C_80620000_k_45000_c_8000.csv &
+
+./germany_partners-rel -i agent_input_80620000.csv -s $RANDOM -k 45000 -c 10000 -a C >output_fixed_C_80620000_k_45000_c_10000.csv &
+
+./germany_partners-rel -i agent_input_80620000.csv -s $RANDOM -k 50000 -c 10000 -a C >output_fixed_C_80620000_k_50000_c_10000.csv &
 
 ./germany_partners-rel -i agent_input_80620000.csv -s $RANDOM -k 50000 -c 20000 -a C >output_fixed_C_80620000_k_50000_c_20000.csv &
 
-./germany_partners-rel -i agent_input_80620000.csv -s $RANDOM -k 50000 -a D >output_fixed_D_80620000_k_50000.csv &
+# ./germany_partners-rel -i agent_input_80620000.csv -s $RANDOM -k 5000 -a D >output_fixed_D_80620000_k_5000.csv &
+
+# ./germany_partners-rel -i agent_input_80620000.csv -s $RANDOM -k 10000 -a D >output_fixed_D_80620000_k_10000.csv &
 
 wait
 
