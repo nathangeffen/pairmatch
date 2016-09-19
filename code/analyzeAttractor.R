f44afcca1aee86a1e528d2b18b41a7e2be91c444
Nathan Geffen <nathangeffen@gmail.com>
2016-09-19 22:14:51 +0200

    Corrected one of the R analyzers.

--- code/analyzeAttractor.R
+++ code/analyzeAttractor.R
@@ -9,62 +9,73 @@ df100 = read.csv("output5k_ra_100.csv", comment.char = "#")
 
 results = df000
 result_rows = results[grepl("mean distance", results$Measure),]
-df = data.frame(Algorithm=result_rows$Algorithm) 
-df["Mean 0.00"] = result_rows$Value
-df["Ratio to best 0.00"] = result_rows$Value / min(result_rows$Value)
+result_rows = aggregate(result_rows$Value,by=list(result_rows$Algorithm),FUN=mean)
+df = data.frame(Algorithm=result_rows$Group.1) 
+df["Mean 0.00"] = result_rows$x
+df["Ratio to best 0.00"] = result_rows$x / min(result_rows$x)
 
 results = df025
 result_rows = results[grepl("mean distance", results$Measure),]
-df["Mean 0.25"] = result_rows$Value
-df["Ratio to best 0.25"] = result_rows$Value / min(result_rows$Value)
+result_rows = aggregate(result_rows$Value,by=list(result_rows$Algorithm),FUN=mean)
+df["Mean 0.25"] = result_rows$x
+df["Ratio to best 0.25"] = result_rows$x / min(result_rows$x)
 
 results = df050
 result_rows = results[grepl("mean distance", results$Measure),]
-df["Mean 0.50"] = result_rows$Value
-df["Ratio to best 0.50"] = result_rows$Value / min(result_rows$Value)
+result_rows = aggregate(result_rows$Value,by=list(result_rows$Algorithm),FUN=mean)
+df["Mean 0.50"] = result_rows$x
+df["Ratio to best 0.50"] = result_rows$x / min(result_rows$x)
 
 results = df075
 result_rows = results[grepl("mean distance", results$Measure),]
-df["Mean 0.75"] = result_rows$Value
-df["Ratio to best 0.75"] = result_rows$Value / min(result_rows$Value)
+result_rows = aggregate(result_rows$Value,by=list(result_rows$Algorithm),FUN=mean)
+df["Mean 0.75"] = result_rows$x
+df["Ratio to best 0.75"] = result_rows$x / min(result_rows$x)
 
 results = df100
 result_rows = results[grepl("mean distance", results$Measure),]
-df["Mean 1.00"] = result_rows$Value
-df["Ratio to best 1.00"] = result_rows$Value / min(result_rows$Value)
+result_rows = aggregate(result_rows$Value,by=list(result_rows$Algorithm),FUN=mean)
+df["Mean 1.00"] = result_rows$x
+df["Ratio to best 1.00"] = result_rows$x / min(result_rows$x)
 
 df = df[order(df["Ratio to best 0.50"]),]
 print(df)
-xtable(df)
+xtable(df,digits=4)
 
 # Rankings
 
 results = df000
 result_rows = results[grepl("mean rank", results$Measure),]
-df = data.frame(Algorithm=result_rows$Algorithm) 
-df["Mean 0.00"] = result_rows$Value
-df["Ratio to best 0.00"] = result_rows$Value / min(result_rows$Value)
+result_rows = aggregate(result_rows$Value,by=list(result_rows$Algorithm),FUN=mean)
+df = data.frame(Algorithm=result_rows$Group.1) 
+df["Mean 0.00"] = result_rows$x
+df["Ratio to best 0.00"] = result_rows$x / min(result_rows$x)
 
 results = df025
 result_rows = results[grepl("mean rank", results$Measure),]
-df["Mean 0.25"] = result_rows$Value
-df["Ratio to best 0.25"] = result_rows$Value / min(result_rows$Value)
+result_rows = aggregate(result_rows$Value,by=list(result_rows$Algorithm),FUN=mean)
+df["Mean 0.25"] = result_rows$x
+df["Ratio to best 0.25"] = result_rows$x / min(result_rows$x)
 
 results = df050
 result_rows = results[grepl("mean rank", results$Measure),]
-df["Mean 0.50"] = result_rows$Value
-df["Ratio to best 0.50"] = result_rows$Value / min(result_rows$Value)
+result_rows = aggregate(result_rows$Value,by=list(result_rows$Algorithm),FUN=mean)
+df["Mean 0.50"] = result_rows$x
+df["Ratio to best 0.50"] = result_rows$x / min(result_rows$x)
 
 results = df075
 result_rows = results[grepl("mean rank", results$Measure),]
-df["Mean 0.75"] = result_rows$Value
-df["Ratio to best 0.75"] = result_rows$Value / min(result_rows$Value)
+result_rows = aggregate(result_rows$Value,by=list(result_rows$Algorithm),FUN=mean)
+df["Mean 0.75"] = result_rows$x
+df["Ratio to best 0.75"] = result_rows$x / min(result_rows$x)
 
 results = df100
 result_rows = results[grepl("mean rank", results$Measure),]
-df["Mean 1.00"] = result_rows$Value
-df["Ratio to best 1.00"] = result_rows$Value / min(result_rows$Value)
+result_rows = aggregate(result_rows$Value,by=list(result_rows$Algorithm),FUN=mean)
+df["Mean 1.00"] = result_rows$x
+df["Ratio to best 1.00"] = result_rows$x / min(result_rows$x)
 
 df = df[order(df["Ratio to best 0.50"]),]
+
 print(df)
 xtable(df)
